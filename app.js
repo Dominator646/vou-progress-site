5c481ca95585992c072ca4ce5a872d239ac8e5da
Dominator646 <nikitapopow643@gmail.com>
2026-05-11 14:13:44 +0300

    Update app.js

--- app.js
+++ app.js
@@ -1,53 +1,62 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+import { createClient }
 
-import {
-getFirestore,
-doc,
-onSnapshot
+from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
+const supabaseUrl =
+"sb_publishable_mdFgb8jLvqq_EWASh7koxw_dftx3lfd";
+const supabaseKey =
+"eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InR0bndneWFvb2pwdXZnZG13ZmVhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0OTE2MDIsImV4cCI6MjA5NDA2NzYwMn0.TT9d50p3BVLFJ7LeUzsYdcpBO-7dXoV4fxkJnvlx-XU";
+const supabase =
+createClient(
+    supabaseUrl,
+    supabaseKey
+);
+const fill =
+document.getElementById("fill");
+const percent =
+document.getElementById("percent");
+const title =
+document.getElementById("title");
+const almost =
+document.getElementById("almost");
+async function loadData(){
+    const { data } =
+    await supabase
+    .from("site_data")
+    .select("*")
+    .eq("id",1)
+    .single();
+    updateUI(data);
 }
-from "https://www.gstatic.com/firebasejs/10.12.2/firebase-firestore.js";
-
-const firebaseConfig = {
-
-  apiKey: "ТВОЙ_KEY",
-  authDomain: "PROJECT.firebaseapp.com",
-  projectId: "PROJECT_ID"
-
-};
-
-const app = initializeApp(firebaseConfig);
-
-const db = getFirestore(app);
-
-const fill = document.getElementById("fill");
-const percent = document.getElementById("percent");
-const title = document.getElementById("title");
-const online = document.getElementById("onlineCount");
-const status = document.getElementById("serverStatus");
-const almost = document.getElementById("almostReady");
-
-setTimeout(()=>{
-    document.getElementById("loading").style.display="none";
-},2500);
-
-onSnapshot(doc(db,"site","progress"),(snap)=>{
-
-    const data = snap.data();
-
-    fill.style.width = data.percent + "%";
-
-    percent.innerText = data.percent + "%";
-
-    title.innerText = data.title;
-
-    online.innerText = data.online;
-
-    status.innerText = data.status;
-
-    if(data.percent >= 90){
-        almost.style.display = "block";
+function updateUI(data){
+    fill.style.width =
+    data.progress + "%";
+    percent.innerText =
+    title.innerText =
+    data.title;
+    if(data.progress >= 90){
+        almost.style.display =
+        "block";
     }else{
-        almost.style.display = "none";
+        "none";
     }
-
-});
+supabase
+.channel("site_data_changes")
+.on(
+"postgres_changes",
+{
+event:"UPDATE",
+schema:"public",
+table:"site_data"
+},
+(payload)=>{
+    updateUI(
+        payload.new
+    );
+})
+.subscribe();
+loadData();
+setTimeout(()=>{
+    document.getElementById(
+        "loading"
+    ).style.display = "none";
+},1500);
